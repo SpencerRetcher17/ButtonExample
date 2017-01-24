98ba56df5053d0e6950eedaf7a91976cdf0c3269
SpencerRetcher17 <retchers@bishopireton.org>
2017-01-23 20:12:10 -0500

    Button Listeners

---
+++ app/src/main/java/com/example/sretcher/buttonexample/Example.java
@@ -0,0 +1,8 @@
+package com.example.sretcher.buttonexample;
+
+/**
+ * Created by sretcher on 1/6/2017.
+ */
+
+public class Example {
+}
---
+++ app/src/main/java/com/example/sretcher/buttonexample/MainActivity.java
@@ -0,0 +1,155 @@
+package com.example.sretcher.buttonexample;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+import android.content.Intent;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button spencerButton = (Button) findViewById(R.id.Button1);
+
+        spencerButton.setOnClickListener(
+
+                new Button.OnClickListener() {
+
+                    public void onClick(View v) {
+
+                        TextView spencerTextView = (TextView) findViewById(R.id.largeText1);
+
+                        spencerTextView.setText("I have changed the text");
+
+
+                    }
+                }
+        );
+
+        spencerButton.setOnLongClickListener(
+
+        new Button.OnLongClickListener() {
+
+            public boolean onLongClick(View v) {
+
+                TextView spencerTextView = (TextView) findViewById(R.id.largeText1);
+
+                spencerTextView.setText("WHOOOOOOO");
+
+                return true;
+            }
+        }
+        );
+
+
+
+
+
+        Button nextButton = (Button) findViewById(R.id.advanceButton);
+
+        nextButton.setOnClickListener(
+                new Button.OnClickListener() {
+
+                    public void onClick(View v) {
+
+                        Intent intent1=new Intent(MainActivity.this,Main2Activity.class);
+
+                    startActivity(intent1);
+
+                    }
+
+
+                }
+
+
+        );
+
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
